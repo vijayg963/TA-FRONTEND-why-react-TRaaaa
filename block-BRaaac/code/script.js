abf4ae36325f362f3765b200bd2ddee78533314b
Vijay Gupta <vijayg963@gmail.com>
2022-06-05 19:54:06 +0530

    createElement

---
+++ block-BRaaac/code/script.js
@@ -0,0 +1,102 @@
+let input = document.querySelector('input[type="text"]');
+let rootElm = document.querySelector('.movie-list');
+
+let allMovies = [
+  {
+    name: 'Inception',
+    watched: false,
+  },
+  {
+    name: 'Rush',
+    watched: false,
+  },
+];
+
+// create UI
+input.addEventListener('keydown', (event) => {
+  if (event.keyCode === 13) {
+    allMovies.push({
+      name: event.target.value,
+      watched: false,
+    });
+    event.target.value = '';
+    createMovieUI();
+  }
+});
+
+function deleteMovie(id) {
+  allMovies.splice(id, 1);
+  createMovieUI();
+}
+
+function handleChange(event) {
+  let id = event.target.id;
+  allMovies[id].watched = !allMovies[id].watched;
+}
+
+function elm(type, attr = {}, ...children) {
+  let element = document.createElement(type);
+  for (let key in attr) {
+    if (key.startsWith('data-')) {
+      element.setAttribute(key, attr[key]);
+    } else {
+      element[key] = attr[key];
+    }
+  }
+  children.forEach((child) => {
+    if (typeof child === 'object') {
+      element.append(child);
+    }
+    if (typeof child === 'string') {
+      let node = document.createTextNode(child);
+      element.append(node);
+    }
+  });
+  return element;
+}
+
+function createMovieUI(data = allMovies) {
+  rootElm.innerHTML = '';
+  data.forEach((movie, i) => {
+    let li = elm(
+      'li',
+      { className: 'flex' },
+      elm('input', { type: 'checkbox', id: i, checked: movie.watched }),
+      elm('label', { for: i }, movie.name),
+      elm('span', { 'data-id': i }, '❌')
+    );
+    rootElm.append(li);
+  });
+}
+
+document.querySelector('.movie-list').addEventListener('click', (e) => {
+  const { tagName } = e.target;
+  const { id } = e.target.dataset;
+  if (tagName === 'SPAN') {
+    deleteMovie(id);
+  }
+});
+
+// function createMovieUI(allMovies, rootElm) {
+//   allMovies.forEach((movie, i) => {
+//     let li = elm('li');
+//     li.className = 'flex';
+//     let input = elm('input');
+//     input.type = 'checkbox';
+//     input.id = i;
+//     input.checked = movie.watched;
+//     input.addEventListener('change', handleChange);
+//     let label = elm('label');
+//     label.for = i;
+//     label.innerText = movie.name;
+//     let span = elm('span');
+//     span.innerText = '❌';
+//     span.setAttribute('data-id', i);
+//     span.addEventListener('click', deleteMovie);
+
+//     li.append(input, label, span);
+//     rootElm.append(li);
+//   });
+// }
+
+createMovieUI();
