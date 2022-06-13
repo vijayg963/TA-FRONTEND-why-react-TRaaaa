ea43041e57e634892974b26ccd86f5b1881ceec5
Vijay Gupta <vijayg963@gmail.com>
2022-06-14 00:19:50 +0530

    Color-Pallet

---
+++ block-BRaaay/color-pallet/src/App.js
@@ -0,0 +1,12 @@
+import './stylesheet/index.css';
+import Colors from './Color';
+
+function App() {
+  return (
+    <div className='App'>
+      <Colors />
+    </div>
+  );
+}
+
+export default App;
